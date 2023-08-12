aceef7c6419a28702a36f2680c86315a743ea3ce
Dave Prades <daveprades80@gmail.com>
2023-08-12 19:04:29 +0700

    add content

--- pages/index.vue
+++ pages/index.vue
@@ -66,6 +66,7 @@
                     <!-- gmail -->
                     <a href="https://mail.google.com/mail/u/0/#inbox" target="_blank" class="ml-2 mt-2">
                       <button class="hover-red rounded-lg p-2">
+                        <!-- <span class="gmail-icon"></span> -->
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                           <path fill="black" d="m18.73 5.41l-1.28 1L12 10.46L6.55 6.37l-1.28-1A2 2 0 0 0 2 7.05v11.59A1.36 1.36 0 0 0 3.36 20h3.19v-7.72L12 16.37l5.45-4.09V20h3.19A1.36 1.36 0 0 0 22 18.64V7.05a2 2 0 0 0-3.27-1.64z"/>
                           </svg>
