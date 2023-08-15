6c6fdc8c0bbef628029b171572f198705c26a03a
Dave Prades <daveprades80@gmail.com>
2023-08-15 21:22:59 +0700

    remove border in contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -58,7 +58,7 @@
         </div>
       </section>
     </div>
-    <footer class="bg-gray-800 text-white py-4 text-center w-screen" style="font-family: 'Poppins';">
+         <footer class="bg-gray-800 text-white py-4 text-center w-screen" style="font-family: 'Poppins';">
           <p>© 2023 My Portfolio. All rights reserved.</p>
         </footer>
 </template>
