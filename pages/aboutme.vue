32b1f477dc1071f824364d924482b03265ab070e
Dave Prades <daveprades80@gmail.com>
2023-08-15 21:20:16 +0700

    set the navbar in to a componet

--- pages/aboutme.vue
+++ pages/aboutme.vue
@@ -1,41 +1,35 @@
 <template>
-     <nav class="navbar fixed top-0 w-full bg-gray-800 h-16 md:h-20">
-      <div class="container mx-auto flex justify-between items-center">
-          <h1 class="text-xl sm:text-2xl font-bold text-center text-white w-full sm:w-auto py-2 px-4 sm:px-0 mt-4" style="font-family: 'Poppins';">
-            Dave Prades
-          </h1>   
-        <nav class="hidden md:flex space-x-10 font-medium font-poppins">
-          <NuxtLink to="/#" a href="#" class="text-white pt-6 ml-4 transition hover:text-blue-300 hover:underline">Home</NuxtLink>
-          <NuxtLink to="/aboutme" a href="#about" class="text-white hover:text-blue-300 pt-6 ml-4 hover:underline">About</NuxtLink>
-          <NuxtLink to="/skills" a href="#skills" class="text-white hover:text-blue-300 pt-6 hover:underline">Skills</NuxtLink>
-          <NuxtLink to="/projects" a href="#projects" class="text-white hover:text-blue-300 pt-6 hover:underline">Projects</NuxtLink>
-          <NuxtLink to="/Contact" a href="#contact" class="text-white hover:text-blue-300 pt-6 hover:underline">Contact</NuxtLink>
-        </nav>    
-      </div>
-      </nav>
-  <section id="about" class="grid grid-cols py-24">
-    <h2 class="text-5xl font-bold font-poppins text-center mb-8 " style="font-family: 'Poppins';">About Me</h2>
-    <div class="flex justify-center">
-      <p class="text-3xl mb-4 p-2 font-semibold sm:text-center md:text-3xl lg:text-3xl xl:text-3xl text-gray-600 text-center" style="font-family: 'Poppins';">
-        Hi There! I'm Dave M. Prades, A Front-End Developer
-      </p>
-    </div>
+   <div> 
+    <navbar />
+    <menubar /> 
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
-      <div class="flex justify-center sm:justify-start mb-6 sm:mb-0 mx-auto">
-        <img class="w-48 sm:w-96 h-48 sm:h-96 border border-gray-400 rounded-full shadow shadow-gray hover:transform hover:scale-110 transition" src="@/assets/img/Dave2x2.png" />
-      </div>
-
-      <div class="mx-auto my-4 py-10 px-4 sm:px-0">
-          <p class="text-base sm:text-base lg:text-base text-justify text-center border border-gray-300 rounded-lg px-6 py-6 animate__animated animate__slideInUp" style="font-family: 'Poppins';">
-          I'm a passionate front-end developer specializing in creating engaging user experiences. With expertise in HTML, CSS, and JavaScript,
-          I transform design concepts into functional websites and applications. I prioritize clean and responsive code, using modern frameworks like Vue.js and Tailwind CSS.
-          I am excited about the prospect of collaborating with you on your next project. By combining my technical proficiency, creativity, and dedication to user-centric design,
-          I am confident that together we can create a remarkable digital solution. Thank you for considering me, and I look forward to the opportunity to contribute to the success of your project.
+    <section id="about" class="grid grid-cols py-24">
+      <h2 class="text-5xl font-bold font-poppins text-center mb-8 " style="font-family: 'Poppins';">About Me</h2>
+      <div class="flex justify-center">
+        <p class="text-3xl mb-4 p-2 font-semibold sm:text-center md:text-3xl lg:text-3xl xl:text-3xl text-gray-600 text-center" style="font-family: 'Poppins';">
+          Hi There! I'm Dave M. Prades, A Front-End Developer
         </p>
       </div>
-    </div>
-  </section>
+
+      <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <div class="flex justify-center sm:justify-start mb-6 sm:mb-0 mx-auto">
+          <img class="w-48 sm:w-96 h-48 sm:h-96 border border-gray-400 rounded-full shadow shadow-gray hover:transform hover:scale-110 transition" src="@/assets/img/Dave2x2.png" />
+        </div>
+
+        <div class="mx-auto my-4 py-10 px-4 sm:px-0">
+            <p class="text-base sm:text-base lg:text-base text-justify text-center border border-gray-300 rounded-lg px-6 py-6 animate__animated animate__slideInUp" style="font-family: 'Poppins';">
+            I'm a passionate front-end developer specializing in creating engaging user experiences. With expertise in HTML, CSS, and JavaScript,
+            I transform design concepts into functional websites and applications. I prioritize clean and responsive code, using modern frameworks like Vue.js and Tailwind CSS.
+            I am excited about the prospect of collaborating with you on your next project. By combining my technical proficiency, creativity, and dedication to user-centric design,
+            I am confident that together we can create a remarkable digital solution. Thank you for considering me, and I look forward to the opportunity to contribute to the success of your project.
+          </p>
+        </div>
+      </div>
+    </section>
+  </div>
+  <footer class="bg-gray-800 text-white py-4 text-center w-screen" style="font-family: 'Poppins';">
+          <p>© 2023 My Portfolio. All rights reserved.</p>
+        </footer>
 </template>
 
 <script>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,80 +1,73 @@
 <template>
-  <nav class="navbar fixed top-0 w-full bg-gray-800 h-16 md:h-20">
-      <div class="container mx-auto flex justify-between items-center">
-          <h1 class="text-xl sm:text-2xl font-bold text-center text-white w-full sm:w-auto py-2 px-4 sm:px-0 mt-4" style="font-family: 'Poppins';">
-            Dave Prades
-          </h1>   
-        <nav class="hidden md:flex space-x-10 font-medium font-poppins">
-          <NuxtLink to="/#" a href="#" class="text-white pt-6 ml-4 transition hover:text-blue-300 hover:underline">Home</NuxtLink>
-          <NuxtLink to="/aboutme" a href="#about" class="text-white hover:text-blue-300 pt-6 ml-4 hover:underline">About</NuxtLink>
-          <NuxtLink to="/skills" a href="#skills" class="text-white hover:text-blue-300 pt-6 hover:underline">Skills</NuxtLink>
-          <NuxtLink to="/projects" a href="#projects" class="text-white hover:text-blue-300 pt-6 hover:underline">Projects</NuxtLink>
-          <NuxtLink to="/contact" a href="#contact" class="text-white hover:text-blue-300 pt-6 hover:underline">Contact</NuxtLink>
-        </nav>    
-      </div>
-      </nav>
+    <div> 
+        <navbar />
+        <menubar />
 
-      <section id="projects" class="w-screen ml-0 py-24">
-        <h2 class="text-4xl font-bold text-center" style="font-family: 'Poppins';">My Projects</h2>
-        <div class="flex flex-col sm:flex-row justify-center animate-fade-up">
-        <div class="flex flex-col sm:flex-row mt-6 sm:mt-12 items-center sm:justify-start">
-            <div class="sm:mb-0">
-            <div class="flex justify-center sm:justify-center">
-                <img class="w-64 h-64 inline-block p-2 zoom-image" src="@/assets/img/road.png" alt="Road Sign Icon">
+        <section id="projects" class="w-screen ml-0 py-24">
+            <h2 class="text-4xl font-bold text-center" style="font-family: 'Poppins';">My Projects</h2>
+            <div class="flex flex-col sm:flex-row justify-center animate-fade-up">
+            <div class="flex flex-col sm:flex-row mt-6 sm:mt-12 items-center sm:justify-start">
+                <div class="sm:mb-0">
+                <div class="flex justify-center sm:justify-center">
+                    <img class="w-64 h-64 inline-block p-2 zoom-image" src="@/assets/img/road.png" alt="Road Sign Icon">
+                </div>
+                </div>
+                <div class="sm:ml-8">
+                <div class="text-justify mt-4 sm:mt-0 rounded-lg px-2 py-4" style="font-family: 'Poppins';">
+                    <p class="mt-4 sm:mt-0 px-4 sm:px-0 text-justify-mobile text-center-desktop" style="font-family: 'Poppins';">
+                    I developed a road sign recognition system  <br class="preserve-line-break" />using Convolutional Neural Networks (CNN). 
+                    <br class="preserve-line-break" />Leveraging deep learning algorithms, the system  <br class="preserve-line-break" />accurately detects and classifies road signs <br>from images. 
+                    This project showcased my expertise  <br class="preserve-line-break" />in computer vision and machine learning,  <br class="preserve-line-break" />achieving high recognition. 
+                    It demonstrated my ability  <br class="preserve-line-break" />to work with complex algorithms  <br class="preserve-line-break" />in real-world scenarios.
+                    </p>
+                </div>
+                </div>
             </div>
             </div>
-            <div class="sm:ml-8">
-            <div class="text-justify mt-4 sm:mt-0 rounded-lg px-2 py-4" style="font-family: 'Poppins';">
+
+            <div class="flex flex-col sm:flex-row mt-6 justify-center">
+            <div class="flex flex-col sm:flex-row mt-6 sm:mt-12 items-center sm:justify-left">
+                <div class="mb-8 sm:mb-0 order-2 sm:order-1">
+                <div class="left-side"> <!-- Removed justify-center sm:justify-start -->
+                    <p class="mt-4 sm:mt-0 px-4 sm:px-0 text-justify-mobile text-center-desktop" style="font-family: 'Poppins';">
+                    The ATM project is an electronic banking outlet
+                    that enables  <br class="preserve-line-break" /> customers to perform basic transactions
+                    without   <br class="preserve-line-break" />the assistance of a branch representative
+                    or teller.  <br class="preserve-line-break" />
+                    ATMs provide convenient self-service transactions
+                    <br class="preserve-line-break" /> such as depositing funds, withdrawing
+                    <br class="preserve-line-break" /> cash, and transferring between accounts.  <br class="preserve-line-break" />
+                    These services are offered directly
+                    by the banks.
+                    </p>
+                </div>
+                </div>
+                <div class="order-1 sm:order-2">
+                <img class="w-72 h-72 inline-block p-2 zoom-image" src="@/assets/img/coding.png" alt="">
+                </div>
+            </div>
+            </div>
+            <div class="flex flex-col sm:flex-row mt-6 justify-center">
+            <div class="grid grid-cols-1 sm:grid-cols-2 mt-6 sm:mt-12 items-center sm:justify-left">
+                <div class="mb-8 sm:mb-0 sm:order-1"> <!-- Add justify-center class to center the image on mobile view -->
+                <img class="w-64 h-64 inline-block p-2 zoom-image" src="@/assets/img/dm.png" alt="">
+                </div>
+
+                <div class="left-side sm:order-2 sm:ml-4">
                 <p class="mt-4 sm:mt-0 px-4 sm:px-0 text-justify-mobile text-center-desktop" style="font-family: 'Poppins';">
-                I developed a road sign recognition system  <br class="preserve-line-break" />using Convolutional Neural Networks (CNN). 
-                <br class="preserve-line-break" />Leveraging deep learning algorithms, the system  <br class="preserve-line-break" />accurately detects and classifies road signs <br>from images. 
-                This project showcased my expertise  <br class="preserve-line-break" />in computer vision and machine learning,  <br class="preserve-line-break" />achieving high recognition. 
-                It demonstrated my ability  <br class="preserve-line-break" />to work with complex algorithms  <br class="preserve-line-break" />in real-world scenarios.
+                    During my On-The Job Training journey, I had the <br class="preserve-line-break" />privilege of contributing to the development <br class="preserve-line-break" />of a 
+                    document maker website designed <br class="preserve-line-break" />specifically for law firm offices. <br class="preserve-line-break" />This robust software simplifies the process of 
+                    <br class="preserve-line-break" />creating legal documents, offering a user-friendly <br class="preserve-line-break" />interface for seamless and efficient document <br>generation. 
+                    It empowers legal professionals to <br class="preserve-line-break" />produce accurate and professional-looking <br class="preserve-line-break" />documents with ease.
                 </p>
+                </div>
             </div>
             </div>
-        </div>
-        </div>
-
-        <div class="flex flex-col sm:flex-row mt-6 justify-center">
-        <div class="flex flex-col sm:flex-row mt-6 sm:mt-12 items-center sm:justify-left">
-            <div class="mb-8 sm:mb-0 order-2 sm:order-1">
-            <div class="left-side"> <!-- Removed justify-center sm:justify-start -->
-                <p class="mt-4 sm:mt-0 px-4 sm:px-0 text-justify-mobile text-center-desktop" style="font-family: 'Poppins';">
-                The ATM project is an electronic banking outlet
-                that enables  <br class="preserve-line-break" /> customers to perform basic transactions
-                without   <br class="preserve-line-break" />the assistance of a branch representative
-                or teller.  <br class="preserve-line-break" />
-                ATMs provide convenient self-service transactions
-                <br class="preserve-line-break" /> such as depositing funds, withdrawing
-                <br class="preserve-line-break" /> cash, and transferring between accounts.  <br class="preserve-line-break" />
-                These services are offered directly
-                by the banks.
-                </p>
-            </div>
-            </div>
-            <div class="order-1 sm:order-2">
-            <img class="w-72 h-72 inline-block p-2 zoom-image" src="@/assets/img/coding.png" alt="">
-            </div>
-        </div>
-        </div>
-        <div class="flex flex-col sm:flex-row mt-6 justify-center">
-        <div class="grid grid-cols-1 sm:grid-cols-2 mt-6 sm:mt-12 items-center sm:justify-left">
-            <div class="mb-8 sm:mb-0 sm:order-1"> <!-- Add justify-center class to center the image on mobile view -->
-            <img class="w-64 h-64 inline-block p-2 zoom-image" src="@/assets/img/dm.png" alt="">
-            </div>
-
-            <div class="left-side sm:order-2 sm:ml-4">
-            <p class="mt-4 sm:mt-0 px-4 sm:px-0 text-justify-mobile text-center-desktop" style="font-family: 'Poppins';">
-                During my On-The Job Training journey, I had the <br class="preserve-line-break" />privilege of contributing to the development <br class="preserve-line-break" />of a 
-                document maker website designed <br class="preserve-line-break" />specifically for law firm offices. <br class="preserve-line-break" />This robust software simplifies the process of 
-                <br class="preserve-line-break" />creating legal documents, offering a user-friendly <br class="preserve-line-break" />interface for seamless and efficient document <br>generation. 
-                It empowers legal professionals to <br class="preserve-line-break" />produce accurate and professional-looking <br class="preserve-line-break" />documents with ease.
-            </p>
-            </div>
-        </div>
-        </div>
-    </section>
+        </section>
+    </div>
+    <footer class="bg-gray-800 text-white py-4 text-center w-screen" style="font-family: 'Poppins';">
+          <p>© 2023 My Portfolio. All rights reserved.</p>
+        </footer>
 </template>
 <script>
 </script>
