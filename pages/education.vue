3c4bcd46f9df1b46f913e36e9d6ddcc3e9e8bc85
Dave Prades <daveprades80@gmail.com>
2023-08-16 21:46:51 +0700

    add hover effect

--- pages/education.vue
+++ pages/education.vue
@@ -3,36 +3,51 @@
      <navbar />
      <menubar /> 
  
-     <div id="education" class="grid grid-cols py-12">
+     <section id="education" class=" w-screen h-full grid grid-cols py-12">
        <h2 class="text-5xl font-bold font-poppins text-center" style="font-family: 'Poppins';">Education</h2>
-     </div>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 flex jutify-center">
-        <!-- Education Item 1 -->
-        <div class="bg-gray-800 text-white border p-6 shadow-md ml-8 mr-8 zoom-in">
-            <h3 class="text-xl font-semibold mb-2" style="font-family: 'Poppins';">Camarines Sur Polytechnic College</h3>
-            <p class="text-white mb-2" style="font-family: 'Poppins';">Bachelor of Science in Computer Science</p>
-            <p class="text-white">Graduated: July 2023</p>
+        <div class="grid grid-cols-1 md:grid-cols-2 flex jutify-center">
+            <!-- Education Item 1 -->
+            <div class="text-white p-2 md:p-6 ml-8 mr-8">
+                <div class="border shadow-md py-8 bg-gray-800 p-4 transition duration-300 hover:bg-gray-900">
+                    <h3 class="text-xl font-semibold" style="font-family: 'Poppins';">Camarines Sur Polytechnic College</h3>
+                    <p class="text-white mb-2" style="font-family: 'Poppins';">Bachelor of Science in Computer Science</p>
+                    <p class="text-white" style="font-family: 'Poppins';">Graduated: July 2023</p>
+                </div>
+            </div>
+
+
+            <!-- Education Item 2 -->
+            <div class="text-white p-2 md:p-6 ml-8 mr-8">
+                <div class="border shadow-md py-8 bg-gray-800 p-4 transition duration-300 hover:bg-gray-900">
+                    <h3 class="text-xl font-semibold" style="font-family: 'Poppins';">Victor Bernal National High School</h3>
+                    <p class="text-white mb-2" style="font-family: 'Poppins';">General Academic Strand (Senior High School)</p>
+                    <p class="text-white" style="font-family: 'Poppins';">Graduated: April 2019</p>
+                </div>
+            </div>
+
+            <!-- Education Item 3 -->
+            <div class="text-white p-2 md:p-6 ml-8 mr-8">
+                <div class="border shadow-md py-8 bg-gray-800 p-4 transition duration-300 hover:bg-gray-900">
+                    <h3 class="text-xl font-semibold" style="font-family: 'Poppins';">Victor Bernal National High School</h3>
+                    <p class="text-white mb-2" style="font-family: 'Poppins';">Junior High School</p>
+                    <p class="text-white" style="font-family: 'Poppins';">Graduated: Arpil 2017</p>
+                </div>
+            </div>
+            <!-- Education Item 4 -->
+            <div class="text-white p-2 md:p-6 ml-8 mr-8">
+                <div class="border shadow-md py-8 bg-gray-800 p-4 transition duration-300 hover:bg-gray-900">
+                    <h3 class="text-xl font-semibold" style="font-family: 'Poppins';">San Vicente Elementary School</h3>
+                    <p class="text-white mb-2" style="font-family: 'Poppins';">Elementary</p>
+                    <p class="text-white" style="font-family: 'Poppins';">Graduated: July 2013</p>
+                </div>
+            </div>
         </div>
-        <!-- Education Item 2 -->
-        <div class="bg-gray-800 text-white border p-6 shadow-md ml-8 mr-8">
-            <h3 class="text-xl font-semibold mb-2" style="font-family: 'Poppins';">Victor Bernal National High School</h3>
-            <p class=" text-white mb-2" style="font-family: 'Poppins';">General Academic Strand (Senior High School)</p>
-            <p class="text-white" style="font-family: 'Poppins';">Graduated: April 2019</p>
-        </div>
-        <!-- Education Item 3 -->
-        <div class="bg-gray-800 text-white border p-6 shadow-md ml-8 mr-8">
-            <h3 class="text-xl font-semibold mb-2" style="font-family: 'Poppins';">Victor Bernal National High School</h3>
-            <p class="text-white mb-2" style="font-family: 'Poppins';">Junior High School</p>
-            <p class="text-white" style="font-family: 'Poppins';">Graduated: April 2017</p>
-        </div>
-        <!-- Education Item 4 -->
-        <div class="bg-gray-800 text-white border p-6 shadow-md ml-8 mr-8">
-            <h3 class="text-xl text-white font-semibold mb-2" style="font-family: 'Poppins';">San Vicente Elementary School</h3>
-            <p class="text-white mb-2" style="font-family: 'Poppins';">Elementary</p>
-            <p class="text-white" style="font-family: 'Poppins';">Graduated: June 2013</p>
-        </div>
-        </div>
+    </section>
     </div>
+
+    <footer class="bg-gray-800 text-white py-4 text-center w-screen" style="font-family: 'Poppins';">
+        <p>© 2023 My Portfolio. All rights reserved.</p>
+    </footer>
  </template>
  
  <script>
