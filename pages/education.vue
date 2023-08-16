394bc4625721eb14a4e119117bdd99e954949d9b
Dave Prades <daveprades80@gmail.com>
2023-08-16 21:47:07 +0700

    remove spacing

--- pages/education.vue
+++ pages/education.vue
@@ -14,8 +14,6 @@
                     <p class="text-white" style="font-family: 'Poppins';">Graduated: July 2023</p>
                 </div>
             </div>
-
-
             <!-- Education Item 2 -->
             <div class="text-white p-2 md:p-6 ml-8 mr-8">
                 <div class="border shadow-md py-8 bg-gray-800 p-4 transition duration-300 hover:bg-gray-900">
@@ -24,7 +22,6 @@
                     <p class="text-white" style="font-family: 'Poppins';">Graduated: April 2019</p>
                 </div>
             </div>
-
             <!-- Education Item 3 -->
             <div class="text-white p-2 md:p-6 ml-8 mr-8">
                 <div class="border shadow-md py-8 bg-gray-800 p-4 transition duration-300 hover:bg-gray-900">
