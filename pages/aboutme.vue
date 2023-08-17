d5fca3cbfa6d8ffe4298653046b231b3303a19c0
Dave Prades <daveprades80@gmail.com>
2023-08-17 21:12:50 +0700

    add shadow effect

--- pages/aboutme.vue
+++ pages/aboutme.vue
@@ -3,7 +3,7 @@
     <navbar />
     <menubar /> 
 
-    <section id="about" class="grid grid-cols py-12">
+    <section id="about" class="py-12">
       <h2 class="text-5xl font-bold font-poppins text-center mb-8 " style="font-family: 'Poppins';">About Me</h2>
       <div class="flex justify-center">
         <p class="text-3xl mb-4 p-2 font-semibold sm:text-center md:text-3xl lg:text-3xl xl:text-3xl text-gray-600 text-center" style="font-family: 'Poppins';">
@@ -11,13 +11,12 @@
         </p>
       </div>
 
-      <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
-        <div class="flex justify-center sm:justify-start mb-6 sm:mb-0 mx-auto">
-          <img class="w-48 sm:w-96 h-48 sm:h-96 border border-gray-400 rounded-full shadow shadow-gray hover:transform hover:scale-110 transition" src="@/assets/img/Dave2x2.png" />
+      <div class="grid grid-cols-1 sm:grid-cols-2">
+        <div class="flex justify-center sm:justify-end mb-6 sm:mb-0 mx-auto">
+          <img class="w-48 sm:w-96 h-48 sm:h-96 shadow shadow-gray-400" src="@/assets/img/Dave2x2.png" />
         </div>
-
-        <div class="mx-auto my-4 py-10 px-4 sm:px-0">
-            <p class="text-base sm:text-base lg:text-base text-justify text-center border border-gray-300 rounded-lg px-6 py-6 animate__animated animate__slideInUp" style="font-family: 'Poppins';">
+        <div class="flex items-center mx-auto">
+          <p class="text-base sm:text-base lg:text-base text-justify px-6 py-6 sm:mx-auto" style="font-family: 'Poppins';">
             I'm a passionate front-end developer specializing in creating engaging user experiences. With expertise in HTML, CSS, and JavaScript,
             I transform design concepts into functional websites and applications. I prioritize clean and responsive code, using modern frameworks like Vue.js and Tailwind CSS.
             I am excited about the prospect of collaborating with you on your next project. By combining my technical proficiency, creativity, and dedication to user-centric design,
@@ -25,11 +24,13 @@
           </p>
         </div>
       </div>
+
+
     </section>
   </div>
   <footer class="bg-gray-800 text-white py-4 text-center w-screen" style="font-family: 'Poppins';">
-    <p>© 2023 My Portfolio. All rights reserved.</p>
-  </footer>
+          <p>© 2023 My Portfolio. All rights reserved.</p>
+        </footer>
 </template>
 
 <script>
