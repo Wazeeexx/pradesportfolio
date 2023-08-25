0bbf5072111e52b8dec28a877453e908d6bcb07d
Dave Prades <daveprades80@gmail.com>
2023-08-25 20:27:36 +0700

    hide img when scrolling

--- pages/projects.vue
+++ pages/projects.vue
@@ -3,23 +3,24 @@
         <navbar />
         <menubar />
 
-        <section id="projects" class="w-screen ml-0 py-12">
+        <section id="projects" class="w-fit ml-0 py-12">
             <h2 class="text-5xl font-bold text-center" style="font-family: 'Poppins';">My Projects</h2>
-            <div class="flex flex-col sm:flex-row justify-center animate-fade-up">
+            <div class="flex flex-col sm:flex-row justify-center animate-fade-up text-center">
                 <div class="flex flex-col sm:flex-row mt-6 sm:mt-12 items-center sm:justify-start">
-                    <button href="" class="image-container">
-                            <div class="overlay">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 48 48"><g fill="white" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"><path d="M27 9V21H39"/><path d="M21 39V27H9"/><path d="M27 21L42 6"/><path d="M21 27L6 42"/></g></svg>
-                            </div>
-                        <div class="flex justify-center sm:justify-center">
-                            <img class="w-2/3 inline-block p-2 " src="@/assets/img/prototype.png" alt="">
+                    <a href="https://github.com/Wazeeexx/Recognition-of-Road-Sign-Using-CNN" class="image-container">
+                        <div class="overlay">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 48 48"><g fill="white" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"><path d="M27 9V21H39"/><path d="M21 39V27H9"/><path d="M27 21L42 6"/><path d="M21 27L6 42"/></g></svg>
                         </div>
-                    </button>
+                        <div class="flex justify-center sm:justify-center">
+                            <img id="scrollingImage" class="w-2/3 inline-block p-2 sm:mx-auto" src="@/assets/img/prototype.png" alt="">
+                        </div>
+                    </a>
                 </div>
             </div>
+
             <div class="sm:ml-8 flex justify-center ">
                 <div class="text-justify mt-4 sm:mt-0 rounded-lg px-2 py-4 max-w-lg" style="font-family: 'Poppins';">
-                    <p class="mt-12" style="font-family: 'Poppins';">
+                    <p class="mt-8" style="font-family: 'Poppins';">
                     I developed a road sign recognition system using Convolutional Neural Networks (CNN). 
                     Leveraging deep learning algorithms, the system accurately detects and classifies road signs from images. 
                     This project showcased my expertise in computer vision and machine learning, achieving high recognition. 
@@ -33,21 +34,21 @@
             <div class="flex flex-col sm:flex-row justify-center animate-fade-up">
                 <div class="flex flex-col sm:flex-row mt-6 sm:mt-12 items-center sm:justify-start">
                     <div class="sm:mb-0">
-                        <button href="https://github.com/Wazeeexx/ATM-Management-System" class="image-container">
+                        <a href="https://github.com/Wazeeexx/ATM-Management-System" class="image-container">
                             <div class="overlay">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 48 48"><g fill="white" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"><path d="M27 9V21H39"/><path d="M21 39V27H9"/><path d="M27 21L42 6"/><path d="M21 27L6 42"/></g></svg>
                             </div>
                             <div class="flex justify-center">
-                                <img class="w-3/4 p-2" src="@/assets/img/atm1.png" alt="">
+                                <img id="scrollingImage" class="w-2/3 inline-block p-2 sm:mx-auto scrolling-image" src="@/assets/img/atm1.png" alt="">
                             </div>
-                        </button>
+                        </a>
                     </div>
                 </div>
             </div>  
 
             <div class="sm:ml-8 flex justify-center ">
                 <div class="text-justify mt-4 sm:mt-0 rounded-lg px-2 py-4 max-w-lg" style="font-family: 'Poppins';">
-                    <p class="mt-12" style="font-family: 'Poppins';">
+                    <p class="mt-8" style="font-family: 'Poppins';">
                     The ATM project is an electronic banking outlet
                     that enables customers to perform basic transactions
                     without the assistance of a branch representative
@@ -70,7 +71,7 @@
                             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 48 48"><g fill="white" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"><path d="M27 9V21H39"/><path d="M21 39V27H9"/><path d="M27 21L42 6"/><path d="M21 27L6 42"/></g></svg>
                         </div>
                         <div class="mb-8 sm:mb-0 sm:order-1 flex justify-center">
-                            <img class="w-3/4 inline-block p-2" src="@/assets/img/document.png" alt="">
+                            <img id="scrollingImage" class="w-2/3 inline-block p-2 sm:mx-auto"  src="@/assets/img/document.png" alt="">
                         </div> 
                     </button>
                 </div>
@@ -91,8 +92,8 @@
                 <div class="pl-10">
                     <button id="scrollButton" class="flex border rounded-full border-gray-300 px-2 py-2 bg-gray-400">
                         <svg xmlns="http://www.w3.org/2000/svg" class="" width="30" height="30" viewBox="0 0 9 16">
-                            <path fill="currentColor" d="M4.5 14c-.28 0-.5-.22-.5-.5v-9c0-.28.22-.5.5-.5s.5.22.5.5v9c0 .28-.22.5-.5.5Z"/>
-                            <path fill="currentColor" d="M8 7.5a.47.47 0 0 1-.35-.15L4.5 4.2L1.35 7.35c-.2.2-.51.2-.71 0c-.2-.2-.2-.51 0-.71l3.5-3.5c.2-.2.51-.2.71 0l3.5 3.5c.2.2.2.51 0 .71c-.1.1-.23.15-.35.15Z"/>
+                            <path fill="white" d="M4.5 14c-.28 0-.5-.22-.5-.5v-9c0-.28.22-.5.5-.5s.5.22.5.5v9c0 .28-.22.5-.5.5Z"/>
+                            <path fill="white" d="M8 7.5a.47.47 0 0 1-.35-.15L4.5 4.2L1.35 7.35c-.2.2-.51.2-.71 0c-.2-.2-.2-.51 0-.71l3.5-3.5c.2-.2.51-.2.71 0l3.5 3.5c.2.2.2.51 0 .71c-.1.1-.23.15-.35.15Z"/>
                         </svg>
                     </button>
                 </div>
@@ -112,6 +113,27 @@ export default {
       });
     });
   },
+
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      const scrollingImage = document.getElementById('scrollingImage');
+      const navbar = document.querySelector('.navbar'); // Update this selector to match your actual navbar class or ID
+      const scrollThreshold = navbar.getBoundingClientRect().bottom;
+
+      if (window.scrollY > scrollThreshold) {
+        scrollingImage.style.display = 'none';
+      } else {
+        scrollingImage.style.display = 'block';
+      }
+    }
+  }
+
 };
 </script>
 
