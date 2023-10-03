3be5bb9e01efc40c46cf80999a322fa7b8c15b46
Dave Prades <daveprades80@gmail.com>
2023-10-03 19:31:30 +0700

    adjust button

--- pages/projects.vue
+++ pages/projects.vue
@@ -7,22 +7,22 @@
             <div class="flex justify-center">
                 <h2 class="text-5xl font-bold" style="font-family: 'Poppins';">My Projects</h2>
             </div>
-            <div class="flex justify-center pt-6 gap-2">
+            <!-- <div class="flex justify-center pt-6 gap-2">
                 <button class="border rounded-lg py-2 px-2 hover:bg-gray-500 font-semibold"  style="font-family: 'Poppins';">Web App</button>
 
                 <div>
                     <button class="border rounded-lg py-2 px-2 hover:bg-gray-500 font-semibold"  style="font-family: 'Poppins';">Project</button>
                 </div>
-            </div>
+            </div> -->
 
             <div class="flex justify-center mt-10">
                 <div class="col-span-3 sm:col-span-1 flex flex-col sm:flex-row sm:justify-start gap-6">
-                    <div>
+                    <div class="bg-black py-4 px-4 rounded-lg">
                         <button class="image-container" @click="showTemplate = true">
                             <div class="overlay">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="red" d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8z"/></svg>
                             </div>
-                            <p class="font-bold text-1xl pb-2 hover:underline" style="font-family: 'Poppins';">RECOGNIGTION OF ROAD SIGN</p>
+                            <p class="font-bold text-1xl pb-2 hover:underline text-white" style="font-family: 'Poppins';">RECOGNIGTION OF ROAD SIGN</p>
                             <div class="flex justify-center sm:justify-center border border-gray-400 rounded-lg">
                                 <img id="" class="w-96 inline-block p-2 sm:mx-auto" src="@/assets/img/prototype.png" alt="">
                             </div>
@@ -51,17 +51,18 @@
                                         </ul>
                                     </div>
                                 </div>
-                                <button class="modal-close hover:bg-gray-800" style="font-family: 'Poppins';" @click="showTemplate = false">Close</button>
+                                <button class="modal-close btn-18" style="font-family: 'Poppins';" @click="showTemplate = false">Close</button>
                             </div>
                         </div>
                     </div>
- 
+
+                  <div  class="bg-black py-4 px-4 rounded-lg">  
                     <div class="col-span-3 sm:col-span-1 flex flex-col sm:flex-row sm:justify-start">
                         <button class="image-container" @click="toggleModal('atm1Modal')">
                         <div class="overlay">
                             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="red" d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8z"/></svg>
                         </div>
-                        <p class="font-bold text-1xl pb-2 " style="font-family: 'Poppins';">ATM MANAGEMENT SYSTEM</p>
+                        <p class="font-bold text-1xl pb-2 text-white" style="font-family: 'Poppins';">ATM MANAGEMENT SYSTEM</p>
                         <div class="flex justify-center sm:justify-center border border-gray-400 rounded-lg">
                             <img class="w-96 inline-block p-2 sm:mx-auto scrolling-image" src="@/assets/img/atm1.png" alt="">
                         </div>
@@ -92,11 +93,13 @@
                                         </ul>
                                     </div>
                             </div>
-                            <button class="modal-close hover:bg-gray-800" @click="closeModal" style="font-family: 'Poppins';">Close</button>
+                            <button class="modal-close hover:bg-gray-800 btn-18" @click="closeModal" style="font-family: 'Poppins';">Close</button>
                         </div>
                     </div>
                 </div>
+              </div>
 
+              <div class="bg-black py-4 px-4 rounded-lg">
                 <div class="col-span-3 sm:col-span-1 flex flex-col sm:flex-row sm:justify-start">
                     <button class="image-container" id="openModalBtn">
                         <div class="overlay">
@@ -104,7 +107,7 @@
                                 <!-- SVG path here -->
                             </svg>
                         </div>
-                        <p class="font-bold text-1xl pb-2" style="font-family: 'Poppins';">DOCUMENT MAKER</p>
+                        <p class="font-bold text-1xl pb-2 text-white" style="font-family: 'Poppins';">DOCUMENT MAKER</p>
                         <div class="flex justify-center sm:justify-center border border-gray-400 rounded-lg">
                             <img id="modalImage" class="w-80 h-44 inline-block p-2 sm:mx-auto" src="@/assets/img/document.png" alt="">
                         </div>
@@ -120,6 +123,7 @@
                 </div>
               </div>
             </div>
+          </div>
         </section>
     </div>
     
@@ -269,4 +273,142 @@ export default {
   border-radius: 4px;
   float: right;
 }
+
+.btn-18,
+.btn-18 *,
+.btn-18 :after,
+.btn-18 :before,
+.btn-18:after,
+.btn-18:before {
+  border: 0 solid;
+  box-sizing: border-box;
+}
+.btn-18 {
+  -webkit-tap-highlight-color: transparent;
+  -webkit-appearance: button;
+  background-color: #000;
+  background-image: none;
+  color: #fff;
+  cursor: pointer;
+  font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont,
+    Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif,
+    Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+  font-size: 100%;
+  font-weight: 900;
+  line-height: 1.5;
+  margin: 0;
+  -webkit-mask-image: -webkit-radial-gradient(#000, #fff);
+  padding: 0;
+  text-transform: uppercase;
+}
+.btn-18:disabled {
+  cursor: default;
+}
+.btn-18:-moz-focusring {
+  outline: auto;
+}
+.btn-18 svg {
+  display: block;
+  vertical-align: middle;
+}
+.btn-18 [hidden] {
+  display: none;
+}
+.btn-18 {
+  border-radius: 99rem;
+  border-width: 2px;
+  padding: 0.8rem 3rem;
+  z-index: 0;
+}
+.btn-18,
+.btn-18 .text-container {
+  overflow: hidden;
+  position: relative;
+}
+.btn-18 .text-container {
+  display: block;
+}
+.btn-18 .text {
+  display: block;
+  mix-blend-mode: difference;
+  position: relative;
+}
+.btn-18:hover .text {
+  -webkit-animation: move-up-alternate 0.3s forwards;
+  animation: move-up-alternate 0.3s forwards;
+}
+@-webkit-keyframes move-up-alternate {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(80%);
+  }
+  51% {
+    transform: translateY(-80%);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+@keyframes move-up-alternate {
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(80%);
+  }
+  51% {
+    transform: translateY(-80%);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+.btn-18:after,
+.btn-18:before {
+  --tilt: 20px;
+  background: #fff;
+  -webkit-clip-path: polygon(
+    0 0,
+    calc(100% - var(--tilt)) 0,
+    100% 50%,
+    calc(100% - var(--tilt)) 100%,
+    0 100%
+  );
+  clip-path: polygon(
+    0 0,
+    calc(100% - var(--tilt)) 0,
+    100% 50%,
+    calc(100% - var(--tilt)) 100%,
+    0 100%
+  );
+  content: "";
+  height: 100%;
+  left: -100%;
+  position: absolute;
+  top: 0;
+  transition: transform 0.6s;
+  width: 100%;
+}
+.btn-18:after {
+  -webkit-clip-path: polygon(
+    var(--tilt) 0,
+    0 50%,
+    var(--tilt) 100%,
+    100% 100%,
+    100% 0
+  );
+  clip-path: polygon(var(--tilt) 0, 0 50%, var(--tilt) 100%, 100% 100%, 100% 0);
+  left: 100%;
+  z-index: -1;
+}
+.btn-18:hover:before {
+  transform: translateX(100%);
+}
+.btn-18:hover:after {
+  transform: translateX(-100%);
+}
+
+
 </style>
