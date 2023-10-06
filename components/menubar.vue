07a578021e9d5954272116cce6894cdfeb210bfb
Dave Prades <daveprades80@gmail.com>
2023-10-06 11:07:55 +0700

    feat: Change content in about me.

--- components/menubar.vue
+++ components/menubar.vue
@@ -1,36 +1,28 @@
 <template>
     <div>
-        <div class="container mx-auto flex justify-between items-center">
-            <NuxtLink to="/" class="pt-6">
-            <p class="text-2xl font-semibold text-white" style="font-family: 'Poppins';">Dave Prades</p>
-            <!-- <img class="w-14 h-14 md:w-18 md:h-18" src="@/assets/img/logo.png" alt="Logo"> -->
-            </NuxtLink>
-              <!-- <nav class="hidden md:flex space-x-10 font-medium font-poppins pt-4">
-                <NuxtLink to="/#" a href="#" class="text-white pt-4 ml-4 transition hover:text-blue-300 hover:underline">Home</NuxtLink>
-                <NuxtLink to="/aboutme" a href="#about" class="text-white hover:text-blue-300 pt-6 ml-4 hover:underline">About</NuxtLink>
-                <NuxtLink to="/education" a href="#education" class="text-white hover:text-blue-300 pt-6 ml-4 hover:underline">Education</NuxtLink>
-                <NuxtLink to="/skills" a href="#skills" class="text-white hover:text-blue-300 pt-6 hover:underline">Skills</NuxtLink>
-                <NuxtLink to="/projects" a href="#projects" class="text-w hite hover:text-blue-300 pt-6 hover:underline">Projects</NuxtLink>
-              </nav>     -->
-        </div>
-        <div class="dropdown">
-            <button v-on:click="showNavigation = !showNavigation" class="p-2 mt-4 text-white fixed top-0 right-0 z-50 ml-6 md:hidden">
-            <svg class="ml-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                <path fill="#66fcf1" fill-rule="evenodd" d="M3.5 5a1 1 0 0 0 0 2h17a1 1 0 1 0 0-2h-17Zm-1 7a1 1 0 0 1 1-1h17a1 1 0 1 1 0 2h-17a1 1 0 0 1-1-1Zm0 6.001a1 1 0 0 1 1-1h17a1 1 0 1 1 0 2h-17a1 1 0 0 1-1-1Z" clip-rule="evenodd" />
-            </svg>
-            </button>
-            <div v-if="showNavigation" class="flex items-center md:hidden bg-black space-y-4 font-medium font-poppins fixed top-0 right-0 w-full z-40 transform translate-y-0 transition-transform duration-300 ml-6 p-2" style="margin-top: 48px;">
-                <span class="divide-y divide-slate-100">
-                    <NuxtLink to="/#" a href="#" class="block text-white hover:text-blue-300 mt-8" @click="closeNavigation">Home</NuxtLink>
-                    <NuxtLink to="/aboutme" a href="#about" class="block text-white mt-2 pt-2 hover:text-blue-300" @click="closeNavigation">About</NuxtLink>
-                    <NuxtLink to="/education" a href="#education" class="block text-white mt-2 pt-2 hover:text-blue-300">Education</NuxtLink>
-                    <NuxtLink to="/skills" a href="#skills" class="block text-white mt-2 pt-2 hover:text-blue-300" @click="closeNavigation">Skills</NuxtLink>
-                    <NuxtLink to="/projects" a href="#projects" class="block text-white mt-2 pt-2 hover:text-blue-300" @click="closeNavigation">Projects</NuxtLink>
-                    <NuxtLink to="/contact" a href="#contact" class="block text-white mt-2 pt-2 hover:text-blue-300" @click="closeNavigation">Contact</NuxtLink>
-                </span>
-            </div>
-        </div>
+    <div class="container mx-auto flex justify-between items-center">
+      <NuxtLink v-if="!isIndexRoute" to="/" class="pt-6">
+        <p class="text-2xl font-semibold text-white" style="font-family: 'Poppins';">Dave Prades</p>
+      </NuxtLink>
     </div>
+    <div class="dropdown">
+      <button @click="toggleNavigation" class="p-2 mt-4 text-white fixed top-0 right-0 z-50 ml-6 md:hidden">
+        <svg class="ml-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <path fill="#66fcf1" fill-rule="evenodd" d="M3.5 5a1 1 0 0 0 0 2h17a1 1 0 1 0 0-2h-17Zm-1 7a1 1 0 0 1 1-1h17a1 1 0 1 1 0 2h-17a1 1 0 0 1-1-1Zm-1 6.001a1 1 0 0 1 1-1h17a1 1 0 1 1 0 2h-17a1 1 0 0 1-1-1Z" clip-rule="evenodd" />
+        </svg>
+      </button>
+      <div :class="['dropdown-panel', showNavigation ? 'open' : '']">
+        <span class="">
+          <NuxtLink to="/#" a href="#" class="block text-white hover:text-blue-300 mt-8 ml-6" @click="closeNavigation">Home</NuxtLink>
+          <NuxtLink to="/aboutme" a href="#about" class="block text-white mt-2 pt-2 ml-6 hover:text-blue-300" @click="closeNavigation">About</NuxtLink>
+          <NuxtLink to="/education" a href="#education" class="block text-white mt-2 pt-2 ml-6 hover:text-blue-300">Education</NuxtLink>
+          <NuxtLink to="/skills" a href="#skills" class="block text-white mt-2 pt-2 ml-6 hover:text-blue-300" @click="closeNavigation">Skills</NuxtLink>
+          <NuxtLink to="/projects" a href="#projects" class="block text-white mt-2 pt-2 ml-6 hover:text-blue-300" @click="closeNavigation">Projects</NuxtLink>
+          <NuxtLink to="/contact" a href="#contact" class="block text-white mt-2 pt-2 ml-6 hover:text-blue-300" @click="closeNavigation">Contact</NuxtLink>
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -61,9 +53,27 @@ export default {
           this.showNavigation = false; // Close the navigation
         }
       },
-    
+      
+      computed: {
+    // Add a computed property to check if the current route is the index route
+    isIndexRoute() {
+      return this.$route.path === '/';
+    },
+  },
 }
 </script>
 <style>
+.dropdown-panel {
+  position: fixed;
+  top: 0;
+  right: -200px; /* Initially hidden outside the viewport */
+  width: 200px; /* Width of the panel */
+  height: 100%;
+  background-color: #000;
+  transition: right 0.3s ease-in-out;
+}
 
+.dropdown-panel.open {
+  right: 0; /* Slide the panel in */
+}
 </style>
