e4805e6e9ccee00d4c965c88c9b4bd6d2607ba09
Dave Prades <daveprades80@gmail.com>
2023-12-17 09:45:38 +0700

    edit content in contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -10,7 +10,7 @@
         Do you have any project ideas?<br> Feel free to get in touch!</h2>
         <div class="flex justify-center items-center">
 
-        <div class="contact-info-container grid grid-cols-3 gap-6 mt-6">
+        <div class="contact-info-container grid grid-cols-2 gap-6 mt-6">
           <div class="flex flex-col sm:flex-row w-full sm:w-80 border rounded-lg sm:py-4 items-center sm:items-start hover:bg-gray-500" >
             <a class="p-4 sm:w-14 sm:h-14 flex items-center justify-center mb-4 sm:mb-0 sm:mr-4 ml-4 mt-4" style="background-color: #66fcf1;">
               <div class="relative flex justify-right">
@@ -38,19 +38,20 @@
                 <h1 class="text-xs sm:text-sm text-white font-poppins">daveprades80@gmail.com</h1>
               </div>
             </div>
-
-            <div class="flex flex-col sm:flex-row w-full sm:w-80 bg-black border rounded-lg sm:py-4 items-center sm:items-start hover:bg-gray-500">
-              <a class="p-4 sm:w-14 sm:h-14 flex items-center justify-center mb-4 sm:mb-0 sm:mr-4 ml-4 mt-4" style="background-color: #66fcf1;">
-                <div class="relative flex justify-right">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="">
-                    <path fill="black" d="M6 17c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6m9-9a3 3 0 0 1-3 3a3 3 0 0 1-3-3a3 3 0 0 1 3-3a3 3 0 0 1 3 3M3 5v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2Z"/>
-                  </svg>             
-                </div>
-              </a>
-              <div class="flex flex-col justify-center text-center sm:text-left p-2">
-                <span class="text-white font-semibold mb-1 sm:mb-2 mt-4 font-poppins">: Contact No.</span>
-                <h1 class="text-xs sm:text-sm text-white font-poppins">09305126647</h1>
+          </div>
+        </div>
+        <div class="flex justify-center pt-6 px-7">
+          <div class="flex sm:flex-row w-full sm:w-80 bg-black border rounded-lg sm:py-4 items-center sm:items-start hover:bg-gray-500">
+            <a class="p-4 sm:w-14 sm:h-14 flex items-center justify-center mb-4 sm:mb-0 sm:mr-4 ml-4 mt-4" style="background-color: #66fcf1;">
+              <div class="relative flex justify-right">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="">
+                  <path fill="black" d="M6 17c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1H6m9-9a3 3 0 0 1-3 3a3 3 0 0 1-3-3a3 3 0 0 1 3-3a3 3 0 0 1 3 3M3 5v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2Z"/>
+                </svg>             
               </div>
+            </a>
+            <div class="flex flex-col justify-center text-center sm:text-left p-2">
+              <span class="text-white font-semibold mb-1 sm:mb-2 mt-4 font-poppins">: Contact No.</span>
+              <h1 class="text-xs sm:text-sm text-white font-poppins">09305126647</h1>
             </div>
           </div>
         </div>
